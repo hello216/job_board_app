4e9bbecfcc0745c02089c1a0f6059f621bac45bf
Armando <borelliarmando12@gmail.com>
2022-01-02 15:28:53 -0600

    added test home page in frontend

---
+++ frontend/src/views/Home.js
@@ -0,0 +1,14 @@
+// import '../stylesheets/homeStyle.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+export default props => {
+
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  )
+
+}
