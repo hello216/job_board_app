92a46c31af2ae2976e5707632374a73a7ef47546
Armando <borelliarmando12@gmail.com>
2022-01-02 12:21:55 -0600

    deleted secret key from settings file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 
-# this and that
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52ka!s=e@7td496sglqize-p*7m7+k6-31@4d&b8%g)9q!tfc!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
