8e4f9ad5aac9bbd41fcafdaa4b24352d5328510b
Armando <borelliarmando12@gmail.com>
2022-01-02 12:27:59 -0600

    added secret key to settings.py file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
