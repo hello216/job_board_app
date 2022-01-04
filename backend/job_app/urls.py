2aa24120bac579b9c8be2e85d53140f527c9e823
Armando <borelliarmando12@gmail.com>
2022-01-04 00:40:31 -0600

    Deleted some libraries that I don't need from views.py

--- backend/job_app/urls.py
+++ backend/job_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from job_app import views
 
-# https://www.django-rest-framework.org/api-guide/routers/
+
 urlpatterns = [
     path('api/log_user', views.log_user),
     path('api/get_user', views.get_user),
--- backend/job_app/views.py
+++ backend/job_app/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, redirect
 from job_app.models import ValidatorManager, User, Jobs
 from django.contrib import messages
 import bcrypt
@@ -11,10 +10,8 @@ from django.core.paginator import Paginator, EmptyPage
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 import bleach
-from rest_framework import viewsets, serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse, JsonResponse
 from job_app.models import User
 
 @api_view(['GET'])
