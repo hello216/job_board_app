6d705ab1414b85a4aefccbd37d0fd17a41efb278
Armando <borelliarmando12@gmail.com>
2022-01-19 16:10:55 -0600

    Changed auth cookie timeout to 1hr(3600 s)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,7 +87,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
         'LOCATION': 'my_cache_table',
-        'TIMEOUT': '1800'
+        'TIMEOUT': '3600'
     }
 }
 
