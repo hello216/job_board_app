493aece18b809c072dd32c22816d50b2aca3f465
Armando <borelliarmando28@gmail.com>
2024-02-22 02:54:42 -0500

    Added axum and tokio

--- new_backend/src/main.rs
+++ new_backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
