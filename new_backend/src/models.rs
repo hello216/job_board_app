a246a874cb93d15c83fc9b95236061e64c4f29d9
Armando <borelliarmando28@gmail.com>
2024-02-25 13:22:32 -0500

    Added model for creating new users

--- new_backend/src/models.rs
+++ new_backend/src/models.rs
@@ -1,5 +1,14 @@
 use diesel::prelude::*;
+use crate::schema::users;
 
+
+
+#[derive(Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub username: &'a str,
+    pub password: &'a str,
+}
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::users)]
