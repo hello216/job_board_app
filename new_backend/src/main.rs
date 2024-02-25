8a6a4ee399368064febbe5122a15a49a298ec453
Armando <borelliarmando28@gmail.com>
2024-02-25 12:58:55 -0500

    Added DB connection in main.rs

--- new_backend/src/main.rs
+++ new_backend/src/main.rs
@@ -1,5 +1,17 @@
 use axum::{routing::get, Router};
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
 
 #[tokio::main]
 async fn main() {
