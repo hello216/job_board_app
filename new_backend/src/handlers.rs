91980af9f310e24b0a130a1819dda21430594a58
Armando <borelliarmando28@gmail.com>
2024-02-25 17:03:00 -0500

    Added db connection and query for users in db in all_users()

--- new_backend/src/handlers.rs
+++ new_backend/src/handlers.rs
@@ -1,8 +1,25 @@
-// use axum::{routing::get};
-
-
 pub mod user_handlers {
-    pub fn all_users() {
-        format!("This will returned all users");
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+use crate::models::{User, NewUser};
+use crate::schema::users::dsl::*;
+
+
+    pub fn establish_connection() -> PgConnection {
+        dotenv().ok();
+    
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        PgConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    }
+
+    pub fn all_users() -> Result<Vec<User>, diesel::result::Error> {
+        let mut connection = establish_connection();
+    
+        let all_users: Vec<User> = users.load::<User>(&mut connection)?;
+    
+        Ok(all_users)
     }
 }
