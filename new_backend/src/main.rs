0275ebcad26eb4412ddd5daa4e2f15cb72892302
Armando <borelliarmando28@gmail.com>
2024-02-26 10:10:38 -0500

    Added all_users handler

--- new_backend/src/main.rs
+++ new_backend/src/main.rs
@@ -1,16 +1,45 @@
-use axum::{routing::get, Router};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use diesel::prelude::*;
+use crate::models::User;
+use crate::schema::users::dsl::*;
+use diesel::pg::PgConnection;
+use dotenvy::dotenv;
+use std::env;
 
 pub mod models;
 pub mod schema;
-pub mod handlers;
 
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .route("/all_users", get(handlers::all_users()));
+fn establish_connection() -> PgConnection {
+    dotenv().ok();
 
-    let listener = tokio::net::TcpListener::bind("localhost:8000").await.expect("Something wrong in the listener");
-    axum::serve(listener, app).await.expect("Something wrong in the serve fn");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .route("/", web::get().to(index))
+                .route("/all_users", web::get().to(all_users))
+        )
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn all_users() -> impl Responder {
+    let mut connection = establish_connection();
+
+    let _all_users: Vec<User> = users.load::<User>(&mut connection).expect("Failed to load users");
+
+    HttpResponse::Ok().body("Hey there!")
 }
