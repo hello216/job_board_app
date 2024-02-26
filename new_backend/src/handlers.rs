290a65915da8df0f4b2b100a509fd1df8c680987
Armando <borelliarmando28@gmail.com>
2024-02-26 08:05:03 -0500

    Fixed some compiling errors

--- new_backend/src/handlers.rs
+++ new_backend/src/handlers.rs
@@ -1,25 +1,27 @@
-pub mod user_handlers {
-use diesel::pg::PgConnection;
+use axum::{response::Json};
 use diesel::prelude::*;
+use crate::models::User;
+use crate::schema::users::dsl::*;
+use diesel::pg::PgConnection;
 use dotenvy::dotenv;
 use std::env;
-use crate::models::{User, NewUser};
-use crate::schema::users::dsl::*;
 
 
-    pub fn establish_connection() -> PgConnection {
-        dotenv().ok();
-    
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        PgConnection::establish(&database_url)
-            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-    }
+fn establish_connection() -> PgConnection {
+    dotenv().ok();
 
-    pub fn all_users() -> Result<Vec<User>, diesel::result::Error> {
-        let mut connection = establish_connection();
-    
-        let all_users: Vec<User> = users.load::<User>(&mut connection)?;
-    
-        Ok(all_users)
-    }
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub async fn all_users() -> Result<Json<Vec<User>>, diesel::result::Error> {
+    let mut connection = establish_connection();
+
+    let all_users: Vec<User> = match users.load::<User>(&mut connection) {
+        Ok(loaded_users) => loaded_users,
+        Err(e) => return Err(e),
+    };
+
+    Ok(Json(all_users))
 }
