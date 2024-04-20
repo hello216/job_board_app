0ee97d4b4dfdfbc9265ffa41326cc2ad96406eaf
Armando <borelliarmando28@gmail.com>
2024-04-19 21:25:01 -0400

    Added api call

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,21 @@
 use yew::prelude::*;
+use reqwasm::http::Request;
 
+
+async fn fetch_data() -> String {
+    let url = String::from("http://localhost:1234/api/");
+    let response = Request::get(&url)
+        .send()
+        .await
+        .unwrap();
+
+    let data = response
+        .json()
+        .await
+        .expect("Failed to parse JSON response");
+
+    data
+}
 
 #[function_component(App)]
 fn app() -> Html {
