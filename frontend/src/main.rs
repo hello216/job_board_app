eb523020f83d9f3705e92549f8136ea6a7e5d9a7
Armando <borelliarmando28@gmail.com>
2024-04-25 09:01:21 -0400

    Added index rendering in main

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -8,10 +8,18 @@ struct HelloTemplate<'a> {
     name: &'a str,
 }
 
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate<'a> {
+    title: &'a str,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(web::resource("/").to(|| async { HelloTemplate { name: "world" } }))
+        App::new()
+            .service(web::resource("/").to(|| async { HelloTemplate { name: "world" } }))
+            .service(web::resource("/index").to(|| async { IndexTemplate { title: "My Rust App" } }))
     })
     .bind(("127.0.0.1", 9999))?
     .run()
