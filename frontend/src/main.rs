a43118640b013150253e8f4e10ac5742d61031be
Armando <borelliarmando28@gmail.com>
2024-04-26 09:29:26 -0400

    Removed hello functions

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,12 +1,6 @@
 use askama_actix::Template;
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, Responder};
 
-
-#[derive(Template)]
-#[template(path = "hello.html")]
-struct HelloTemplate<'a> {
-    name: &'a str,
-}
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -14,12 +8,17 @@ struct IndexTemplate<'a> {
     title: &'a str,
 }
 
+async fn index() -> impl Responder {
+    IndexTemplate {
+        title: "My Rust App Frontend",
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(web::resource("/").to(|| async { HelloTemplate { name: "world" } }))
-            .service(web::resource("/index").to(|| async { IndexTemplate { title: "My Rust App" } }))
+            .service(web::resource("/").to(index))
     })
     .bind(("127.0.0.1", 9999))?
     .run()
