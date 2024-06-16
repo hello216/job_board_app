9385bae17ce479467821a5aa1643334fecd5bd47
Armando <borelliarmando28@gmail.com>
2024-06-15 22:41:17 -0400

    Enabled antiforgery token again

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -21,7 +21,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-// app.UseAntiforgery();
+app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
