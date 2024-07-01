ead4105341b6b8286d0754865ca3fc7381e3d8d1
Armando <borelliarmando28@gmail.com>
2024-07-01 19:57:55 -0400

    Added create job method

--- App/Controllers/JobController.cs
+++ App/Controllers/JobController.cs
@@ -1,20 +1,59 @@
+using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using App.Models;
+using App.Data;
 
 namespace App.Controllers;
 
 public class JobController : Controller
 {
     private readonly ILogger<JobController> _logger;
-
-    public JobController(ILogger<JobController> logger)
+    private readonly AppDbContext _context;
+    public JobController(ILogger<JobController> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
     
     public IActionResult Create()
     {
         return View();
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Status,Title,Company,Url,Location")] Job job)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Add(job);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Job application added!";
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.LogError(ex, "Failed to create job due to Id collision");
+
+                job.Id = Guid.NewGuid().ToString().Substring(0, 12);
+                try
+                {
+                    _context.Add(job);
+                    await _context.SaveChangesAsync();
+                    TempData["SuccessMessage"] = "Job application added!";
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save the job. Please try again.");
+                }
+            }
+        }
+        
+        return View(job);
     }
 }
