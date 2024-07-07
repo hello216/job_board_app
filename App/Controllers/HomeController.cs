966644d83a80434c57eeaff38cf20b24e86aca3c
Armando <borelliarmando28@gmail.com>
2024-07-07 02:51:15 -0400

    Added JobService interface in HomeController

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -1,31 +1,33 @@
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using System.Diagnostics;
+using App.Services;
 using App.Models;
-using App.Data;
 
-namespace App.Controllers;
-
-public class HomeController : Controller
+namespace App.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly AppDbContext _context;
-
-    public HomeController(ILogger<HomeController> logger, AppDbContext context)
+    public class HomeController : Controller
     {
-        _logger = logger;
-        _context = context;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly IJobService _jobService;
 
-    public async Task<IActionResult> Index()
-    {
-        var jobs = await _context.Jobs.ToListAsync();
-        return View(jobs);
-    }
+        public HomeController(ILogger<HomeController> logger, IJobService jobService)
+        {
+            _logger = logger;
+            _jobService = jobService;
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public async Task<IActionResult> Index()
+        {
+            var jobs = await _jobService.GetAllJobsAsync();
+            return View(jobs);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
