ccfe012c9d721c5d06ae996f59bac0aa663ce6e7
borelli <borelliarmando28@gmail.com>
2025-02-07 06:25:21 -0500

    Switch backend to use sqlite

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
