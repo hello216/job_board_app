aa75582d9449ccaa5b6949425f671432e2ccce6a
borelli <borelliarmando28@gmail.com>
2025-02-08 16:38:52 -0500

    Added jobs table in home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,77 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/jobs/getuserjobs`, {
+          credentials: 'include'
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          if (data.length === 0) {
+            setError('No jobs found. Please add a new job!');
+          } else {
+            setJobs(data);
+            setError(null);
+          }
+        } else {
+          setError('Failed to retrieve jobs');
+        }
+      } catch (error) {
+        console.log(error);
+        setError('Error fetching jobs');
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <div id="home-container">
-      Yooo
+      {error ? (
+        <p style={{ color: 'red' }}>{error}</p>
+      ) : jobs.length === 0 ? (
+        <p style={{ textAlign: 'center' }}>No jobs found. Please add a new job!</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Submitted At</th>
+              <th>Status</th>
+              <th>Title</th>
+              <th>Company</th>
+              <th>URL</th>
+              <th>Location</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs.map((job) => (
+              <tr key={job.id}>
+                <td>{job.createdAt}</td>
+                <td>{job.status}</td>
+                <td>{job.title}</td>
+                <td>{job.company}</td>
+                <td>
+                  <a href={job.url} target="_blank">
+                    {new URL(job.url).host}
+                  </a>
+                </td>
+                <td>{job.location}</td>
+                <td>
+                  <a href={`/edit/${job.id}`}>Edit</a>
+                  <a href={`/delete/${job.id}`}>Delete</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
