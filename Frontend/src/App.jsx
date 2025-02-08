6b1ecdc35d699bd3e9d9dc512200ccf65dd95393
borelli <borelliarmando28@gmail.com>
2025-02-08 00:52:02 -0500

    Added login route in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
