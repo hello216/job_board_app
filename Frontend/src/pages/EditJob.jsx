a02feeca56ef4c409a47fc6384617feaed449824
borelli <borelliarmando28@gmail.com>
2025-02-12 11:19:05 -0500

    Centered text in inputs in all forms

--- Frontend/src/pages/EditJob.jsx
+++ Frontend/src/pages/EditJob.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import ValidateSanitize from '../services/validateSanitizeService';
+import '../css/EditJob.css';
 
 const EditJob = () => {
   const [job, setJob] = useState({
